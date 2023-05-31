9924573a810918012f8bc3ee7c5cccbbc40147e7
williamkmp <williamkurnia123@gmail.com>
2023-05-31 22:32:36 +0700

    [ADD] ServerRequest for AJAX request to server

---
+++ resources/views/components/server-request-script.blade.php
@@ -0,0 +1,53 @@
+<script>
+    function jsonToFormData(jsonData) {
+        const formData = new FormData();
+        for (let key in jsonData) {
+            if (jsonData.hasOwnProperty(key)) {
+                formData.append(key, jsonData[key]);
+            }
+        }
+        return formData;
+    }
+
+    function getCropperImageBlob(cropper) {
+        return new Promise((resolve, reject) => {
+            cropper.result('blob').then(function(blob) {
+                resolve(blob);
+            });
+        });
+    }
+
+    class ServerRequest {
+        static config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        };
+
+        static get(url) {
+            return new Promise((resolve, reject) => {
+                axios.get(url, this.config)
+                    .then(response => {
+                        resolve(response);
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+        }
+
+        static post(url, data) {
+            let formData = jsonToFormData(data);
+            formData.append('_token', `{{ csrf_token() }}`);
+            return new Promise((resolve, reject) => {
+                axios.post(url, data, this.config)
+                    .then(response => {
+                        resolve(response);
+                    })
+                    .catch(error => {
+                        reject(error);
+                    });
+            });
+        }
+    }
+</script>
