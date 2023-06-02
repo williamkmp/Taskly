ae07508efc97e0c8a1e80f059a9d009a84195775
williamkmp <williamkurnia123@gmail.com>
2023-06-02 22:08:56 +0700

    [UPD] showTeam() implement teams and invites var

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Logic\TeamLogic;
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TeamController extends Controller
 {
+    public function __construct(protected TeamLogic $teamLogic)
+    {
+    }
+
     public function showTeam()
     {
-        //TODO: implement this
-        return view("teams");
+        $user = User::find(Auth::user()->id);
+        $teams = $this->teamLogic->getUserTeams($user->id, ["Member", "Owner"]);
+        $invites = $this->teamLogic->getUserTeams($user->id, ["Pending"]);
+
+        return view("teams")
+            ->with("teams", $teams)
+            ->with("invites", $invites);
     }
 }
