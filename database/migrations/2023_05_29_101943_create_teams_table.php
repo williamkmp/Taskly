9e6ad9856c8a748de31aadb02521bd3113640c40
williamkmp <williamkurnia123@gmail.com>
2023-06-02 14:54:10 +0700

    [ADD] teams column: description, image_path

--- database/migrations/2023_05_29_101943_create_teams_table.php
+++ database/migrations/2023_05_29_101943_create_teams_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('description');
+            $table->string('image_path')->nullable()->default(null);
             $table->timestamps();
         });
     }
