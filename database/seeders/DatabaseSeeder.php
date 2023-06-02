696d0625823ac93fb48a8abf35d5fb2035781337
williamkmp <williamkurnia123@gmail.com>
2023-06-02 17:23:16 +0700

    [ADD] TeamSeeder and UserTeamSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            TeamSeeder::class,
+            UserTeamSeeder::class
         ]);
     }
 }
