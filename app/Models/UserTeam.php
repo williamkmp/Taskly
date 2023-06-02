dbd5b69aacef944cb407c8ba916f94e5039b2c44
williamkmp <williamkurnia123@gmail.com>
2023-06-02 17:10:57 +0700

    [UPD] UserTeam model name

--- app/Models/UserTeam.php
+++ app/Models/UserTeam.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class user_team extends Model
+class UserTeam extends Model
 {
     use HasFactory;
 
