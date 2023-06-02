87e8f775da67547dbfc93072dffb3988c3a74dd6
williamkmp <williamkurnia123@gmail.com>
2023-06-02 17:08:31 +0700

    [UPD} file name

---
+++ database/migrations/2023_06_02_072624_create_user_team_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_team', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('team_id')->constrained();
+            $table->enum("status", ["Member", "Owner", "Pending"])->default("Pending");
+            $table->index(['user_id', 'team_id']);
+            $table->unique(['user_id', 'team_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_team');
+    }
+};
