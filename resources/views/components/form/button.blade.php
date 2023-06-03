0d27acded9de948204f23ee26178bfc6f6db7d3a
williamkmp <williamkurnia123@gmail.com>
2023-06-04 02:06:06 +0700

    [ADD} outline button type

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,4 +1,4 @@
-@props(['primary', 'action', 'type', 'id', 'form', 'accept'])
+@props(['primary', 'action', 'type', 'id', 'form', 'accept', 'outline'])
 
 <button @isset($action) onclick="{{ $action }}" @endisset
     @isset($form) form="{{ $form }}" @endisset
@@ -6,6 +6,8 @@
     @isset($id) id="{{ $id }}" @endisset
     @if (isset($primary))
     {{ $attributes->merge(['class' => 'flex items-center justify-center w-full gap-2 px-6 py-1 text-base font-bold border-4 border-black rounded-full bg-black text-white hover:bg-white hover:text-black']) }}>
+    @elseif (isset($outline))
+    {{ $attributes->merge(['class' => 'flex items-center justify-center w-full gap-2 px-6 py-1 text-base font-bold border-4 border-black rounded-full bg-white text-black hover:bg-black hover:text-white']) }}>
     @else
     {{ $attributes->merge(['class' => 'flex items-center justify-center w-full gap-2 px-6 py-1 text-base font-bold border-4 rounded-full text-black bg-slate-300 border-slate-300 hover:bg-black hover:text-white hover:border-black ']) }}>
     @endif
