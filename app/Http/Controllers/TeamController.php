2e037a57f2f9569ed319a7966e16b5062bb5ec81
williamkmp <williamkurnia123@gmail.com>
2023-06-03 22:24:01 +0700

    [ADD] createTeam and showTeam

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -7,8 +7,8 @@ use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Response as HttpResponse;
 
 class TeamController extends Controller
 {
@@ -16,7 +16,44 @@ class TeamController extends Controller
     {
     }
 
-    public function showTeam()
+    // public function createTeam(Request $request)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         "team_name" => "required|min:5|max:30",
+    //         "team_description" => "required|min:5|max:225"
+    //     ]);
+
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->messages(), HttpResponse::HTTP_BAD_REQUEST);
+    //     }
+
+    //     $createdTeam = $this->teamLogic->createTeam(
+    //         Auth::user()->id,
+    //         $request->team_name,
+    //         $request->team_description,
+    //     );
+
+    //     return response()->json(["redirectUrl" => route("viewTeam", ["team_id" => $createdTeam->id])], HttpResponse::HTTP_OK);
+    // }
+
+    public function createTeam(Request $request)
+    {
+         $request->validate([
+            "team_name" => "required|min:5|max:30",
+            "team_description" => "required|min:5|max:225"
+        ]);
+
+        $createdTeam = $this->teamLogic->createTeam(
+            Auth::user()->id,
+            $request->team_name,
+            $request->team_description,
+        );
+
+        return redirect()->route("viewTeam", ['team_id' => $createdTeam->id]);
+    }
+
+
+    public function showTeams()
     {
 
         $user = User::find(Auth::user()->id);
@@ -28,6 +65,14 @@ class TeamController extends Controller
             ->with("invites", $invites);
     }
 
+    public function showTeam($team_id)
+    {
+        $team_id = intval($team_id);
+        $selected_team = Team::find($team_id);
+        return view("team")
+            ->with("team", $selected_team);
+    }
+
     public function search(Request $request)
     {
         $validator = Validator::make($request->all(), ["team_name" => "required"]);
@@ -35,6 +80,7 @@ class TeamController extends Controller
             return redirect()->route("home");
         }
 
+        // @php-ignore
         $request->session()->flash("__old_team_name", $request->team_name);
         $user = User::find(Auth::user()->id);
         $teams = $this->teamLogic->getUserTeams($user->id, ["Member", "Owner"], $request->team_name);
