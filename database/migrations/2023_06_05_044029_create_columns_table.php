cb9361597e251fde72f51149e71f77e326dfa803
williamkmp <williamkurnia123@gmail.com>
2023-06-05 17:24:27 +0700

    [ADD] columns table

---
+++ database/migrations/2023_06_05_044029_create_columns_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('columns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("board_id")->constrained();
+            $table->foreignId("previous_id")->constrained("columns", "id");
+            $table->foreignId("next_id")->constrained("columns", "id");
+            $table->string("name");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('columns');
+    }
+};
