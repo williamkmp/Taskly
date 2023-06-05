c0f49c8d6b9102e91f2a26553f12a61c4d54695d
williamkmp <williamkurnia123@gmail.com>
2023-06-05 17:24:16 +0700

    [ADD] boards tabel

---
+++ database/migrations/2023_06_05_042305_create_boards_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("team_id")->constrained();
+            $table->string("name");
+            $table->string("pattern");
+            $table->string("image_path")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('boards');
+    }
+};
