a3d460d166e6943c5e7397bc2cec8d2f937312e0
williamkmp <williamkurnia123@gmail.com>
2023-06-05 17:23:29 +0700

    [ADD] Board model

---
+++ app/Models/Board.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'pattern',
+        'image_path',
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
