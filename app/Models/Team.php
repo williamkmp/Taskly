e6514dd6aa0d7e6443800786bd80933577d37200
williamkmp <williamkurnia123@gmail.com>
2023-06-05 17:24:00 +0700

    [ADD] boards relation

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -26,6 +26,11 @@ class Team extends Model
         return $this->belongsToMany(User::class, "user_team", "team_id", "user_id")->withPivot("status");
     }
 
+    public function boards()
+    {
+        return $this->hasMany(Board::class);
+    }
+
     public function userRelations()
     {
         return $this->hasMany(UserTeam::class);
