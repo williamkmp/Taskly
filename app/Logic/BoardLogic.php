7028fefd463c511af53a1c7d13c4cfb50dd7d44f
williamkmp <williamkurnia123@gmail.com>
2023-06-06 21:24:53 +0700

    [ADD] PATTERN for BoardLogic

--- app/Logic/BoardLogic.php
+++ app/Logic/BoardLogic.php
@@ -8,22 +8,37 @@ use App\Models\User;
 
 class BoardLogic
 {
-    public function __construct(protected TeamLogic $teamLogic)
-    {
-    }
+    public const PATTERN = [
+        'sunkist',
+        'mini',
+        'sha-la-la',
+        'celestial',
+        'dream',
+        'blue',
+        'purple',
+        'ellegant',
+        'jaipur',
+        'mild',
+        'sunset',
+        'cosmic',
+        'jupiter',
+        'police'
+    ];
 
     public function createBoard(int $team_id, string $board_name)
     {
         $team = Team::find($team_id);
         $teamExist = ($team != null);
+        $pattern = BoardLogic::PATTERN[array_rand(BoardLogic::PATTERN)];
         if (!$teamExist) return null;
 
         $createdBoard = Board::create([
             "team_id" => $team->id,
             "name" => $board_name,
-            "pattern" => $team->pattern
+            "pattern" => $pattern
         ]);
 
         return $createdBoard;
     }
+
 }
