872e90196d7cc9dfb66d144642bf52408167ec5c
williamkmp <williamkurnia123@gmail.com>
2023-06-06 23:12:09 +0700

    [ADD} fillable and prev, next relation

--- app/Models/Column.php
+++ app/Models/Column.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Column extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'board_id',
+        'previous_id',
+        'next_id',
+    ];
+
+    public function board()
+    {
+        return $this->belongsTo(Board::class);
+    }
+
+    public function previousColumn()
+    {
+        return $this->belongsTo(Column::class, 'previous_id');
+    }
+
+    public function nextColumn()
+    {
+        return $this->belongsTo(Column::class, 'next_id');
+    }
 }
