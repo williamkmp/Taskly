34c5ed95f8c9ebd28426e3d3673defc0ed155995
williamkmp <williamkurnia123@gmail.com>
2023-06-07 03:25:21 +0700

    [INIT] Card table

---
+++ app/Models/Card.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'column_id',
+        'name',
+        'description',
+        'previous_id',
+        'next_id',
+    ];
+
+    public function board()
+    {
+        return $this->belongsTo(Column::class);
+    }
+
+    public function previousCard()
+    {
+        return $this->belongsTo(Card::class, 'previous_id');
+    }
+
+    public function nextCard()
+    {
+        return $this->belongsTo(Card::class, 'next_id');
+    }
+}
---
+++ database/migrations/2023_06_06_201031_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("description")->nullable();
+
+            $table->foreignId("column_id")->constrained();
+            $table->unsignedBigInteger("previous_id")->nullable();
+            $table->unsignedBigInteger("next_id")->nullable();
+
+            $table->foreign('previous_id')->references('id')->on('cards');
+            $table->foreign('next_id')->references('id')->on('cards');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
