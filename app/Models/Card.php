6fdc31e525a71bbbfa2b83f878f2e149ccf2aa97
williamkmp <williamkurnia123@gmail.com>
2023-06-09 18:55:10 +0700

    [ADD] cards MtM relation

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -48,4 +48,9 @@ class Card extends Model
     {
         return $this->belongsTo(Card::class, 'next_id');
     }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, "card_user", "card_id", "user_id");
+    }
 }
